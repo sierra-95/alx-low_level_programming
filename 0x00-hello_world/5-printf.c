a65947e2b993575967c225a046dad259fd788889
sierra-95 <michaelmachohi@gmail.com>
2022-09-08 08:00:00 -0700

    h

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,15 @@
  * main-very boring
  * @parametrix: parameter string c
  *Return: Return value is zero
+ 
  */
 int main(void)
 {
-	char c[100] = "with proper grammar, but the outcome is a piece of art,";
+
+	char c[100] = "with proper grammar, but the outcome is a piece of art\n,";
+
+	
 	printf("%s",c);
-	return 0;
+
+	return (0);
 }
