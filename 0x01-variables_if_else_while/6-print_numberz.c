4e0470e2089f057b47976b33f557005a0d793825
sierra-95 <michaelmachohi@gmail.com>
2022-09-09 09:11:08 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int num;
+   for (num = '0'; num <='9'; num++)
+    {
+    	
+        putchar( num);
+    }
+    putchar('\n');
+    
+  
+
+    return 0;
+}
