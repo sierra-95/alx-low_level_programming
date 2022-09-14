79d1f18141ef033b71c94d0b150744624fde6cc4
sierra-95 <michaelmachohi@gmail.com>
2022-09-14 07:02:26 -0700

    g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main ~ check the code
+ * main - check the code
  * Return :Always 0.
  */
 int main(void)
 {
-	_putchar('-');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
