738bbea5d8f3308c79da665e5bab8e180a819e16
sierra-95 <michaelmachohi@gmail.com>
2022-09-14 02:56:56 -0700

    d

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,17 @@
 #include "main.h"
 /**
  *  * add(int, int) ~ fucntion for summation
+ *  int a ~variable (1)
+ *  int b ~ variable (2)
  *   * main ~ check the code
  *    * return (0)
+ *    return (result) ~ the esult is pushed to main fucntion
  *     **/
  
 int add(int a, int b)
 {
 	    int result = a + b;
 	        
-	        return result;
+	        return (result);
 		    
 }
