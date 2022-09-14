821f9d2ae420ac8c0b657bfc06a6bd654220dd9c
sierra-95 <michaelmachohi@gmail.com>
2022-09-14 02:44:27 -0700

    v

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *
+ * *main ~ function main
+ *
+ * *Return (0)
+ *
+ * */
+
+int main(void)
+
+{
+
+		char michael[8] = "_putchar", i;
+
+			
+
+			for(i=0;i<8;i++)
+
+					{
+
+								putchar(michael[i]);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+ *  * add(int, int) ~ fucntion for summation
+ *   * main ~ check the code
+ *    * return (0)
+ *     **/
+ 
+int add(int a, int b)
+{
+	    int result = a + b;
+	        
+	        return result;
+		    
+}
