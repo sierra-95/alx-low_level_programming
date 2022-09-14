70f047a9a8c998bfbf3353e79b812ae3ebb2231a
sierra-95 <michaelmachohi@gmail.com>
2022-09-14 04:02:13 -0700

    g

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+void print_alphabet_x10(void);
+
+/**
+ *
+ * * main function ~ has the content
+ *
+ * * void print_alphabet(void)
+ *
+ * *void
+ *
+ * *Return (0)
+ *
+ * **/
+
+void print_alphabet_x10(void)
+
+{
+
+
+
+		int i=0,j;
+
+		    char mike[26]="abcdefghijklmnopqrstuvwxyz";
+
+
+
+		    	while(i<10)
+
+					{
+
+								for(j=0;j<26;j++)
+
+											{
+
+															putchar(mike[j]);
+
+																		
+
+																	}
+
+										putchar('\n');
+
+												i++;
+
+													}
+
+}
