b134c5de142959f0e6c6483b4fbf8318c5684921
sierra-95 <michaelmachohi@gmail.com>
2022-09-14 07:22:28 -0700

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *
+ * * main -Always execute
+ *
+ * *  print_alphabet-prints lowercase
+ *
+ *
+ *
+ * *Return: Always 0
+ *
+ * */
+
+void print_alphabet(void)
+
+{
+
+
+
+		char c;
+
+
+
+			for(c='a';c<='z';c++)
+
+					{
+
+								putchar(c);
+
+									}
+
+				
+
+}
