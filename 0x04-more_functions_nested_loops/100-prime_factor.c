fad884b422a2b808cfab5a14e386318424dbce70
sierra-95 <michaelmachohi@gmail.com>
2022-09-16 04:02:31 -0700

    d

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the largest prime factor
+ * Return - always 0
+ * */
+ 
+ int main(void)
+ {
+     long number = 612852475143;
+     int inc;
+     
+     while (inc++ < number /2)
+     {
+         if ( number % inc == 0)
+         {
+             number /= 2;
+             continue;
+         }
+         for (inc = 3; inc < number / 2; inc += 2)
+         {
+             if(number % inc ==0)
+             number /= inc;
+         }
+     }
+     return (0);
+ }
