0c781e15d4203e405633b938cb3f5e78374c8a68
sierra-95 <michaelmachohi@gmail.com>
2022-09-16 03:41:59 -0700

    f

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,7 @@
 void positive_or_negative(int i);
 <<<<<<< HEAD
 int largest_number(int , int , int );
-void print_remaining_days(int , int , int );
+void print_remaining_days(int month, int day , int year );
 int convert_day(int , int );
 =======
 int largest_number(int, int, int);
