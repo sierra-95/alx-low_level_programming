120e52cbc36f301868898b71338d4514d9e33343
sierra-95 <michaelmachohi@gmail.com>
2022-09-19 03:29:04 -0700

    f

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *  swap_int- check the code
+ * @a - variable (1)
+ * @b -variable (2)
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+    *a = 42;
+    *b = 98;
+}
