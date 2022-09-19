35b6b252d75eefdbea2c6631ab017dc15860a0db
sierra-95 <michaelmachohi@gmail.com>
2022-09-19 05:03:26 -0700

    d

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string- check the code
+ * @s: variable
+ *Return: No
+ */
+ void rev_string(char *s)
+ {
+     int i = 0, j = 0;
+     char str[500];
+     
+     while(*(s + i))
+     {
+         *(str + i) = *(s + i);
+         i++;
+     }
+     i = i - 1;
+     while (i >= 0)
+     {
+         *(s + i) = *(str + j);
+         j++;
+         i--;
+    }
+ }
