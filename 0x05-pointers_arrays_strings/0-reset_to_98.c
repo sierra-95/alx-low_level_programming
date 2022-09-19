697f2ccce1a7a16f1e0219a659258e90594b07d3
sierra-95 <michaelmachohi@gmail.com>
2022-09-19 04:40:18 -0700

    f

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * reset_to_98(int *n) - function for reset
- *int *n - variable for reset
- * Return: result
+ * reset_to_98 - function for reset
+ *@n: ariable for reset
+ * Return: no
  */
 void reset_to_98(int *n)
 {
-   
-*n=98;
+    *n = 98;
 }
