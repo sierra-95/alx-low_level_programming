01283b1adabb8b1cd61eab93819f2e8bfc7e15e2
sierra-95 <michaelmachohi@gmail.com>
2022-09-19 05:24:40 -0700

    f

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,7 +6,7 @@
  * @b: variable 2
  *Return: No
  */
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
     int i;
     
@@ -14,8 +14,7 @@ void print_array(int *a, int n);
     {
         printf("%d", a[i]);
     
-    
-    if (i < n - 1)
+     if (i < n - 1)
     {
         printf(", ");
     }
