eb0375ec02a015009bf9665c3ea374a5d201d776
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-21 12:54:09 +0300

    f

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- *  * *leet - encodes a string
- *   * @str: string to be encoded
- *    * Return: return encoded string
- *     * */
+* *leet - encodes a string
+* @str: string to be encoded
+* Return: return encoded string
+* */
 char *leet(char *)
 {
-	    int index1 = 0, index2;
-	        char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+int index1 = 0, index2;
+char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 		    
-		    while (str[++index1])
-			        {
-					        for (index2 = 0; index2 <= 7; index2++)
-							        {
-									            if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
-											                str[index] = index + '0';
-										            }
-						    }
-		        return (str);
+while (str[++index1])
+{
+for (index2 = 0; index2 <= 7; index2++)
+{
+if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
+ str[index] = index + '0';
+}
+}
+return (str);
 }
