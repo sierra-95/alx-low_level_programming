9bd8bc3720f0e14c3079c93c797e6cb0d351cb47
sierra-95 <michaelmachohi@gmail.com>
2022-09-21 02:25:23 -0700

    f

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * *cap_string - All first letters changed to  uppercase
+ * @str: parameter to capitalize
+ * Return: return is a character in uppercase
+ * */
+char *cap_string(char *)
+{
+    int index = 0;
+    
+    while (str[++index])
+    {
+        while(!(str[index] >= 'a' && str[index] <= 'z'))
+        index++;
+        if (str[index - 1] == ' ' ||
+                        str[index-1] == '\t' ||
+                        str[index-1] == '\n' ||
+                        str[index-1] == ',' ||
+                        str[index-1] == ';' ||
+                        str[index-1] == '.' ||
+                        str[index-1] == '!' ||
+                        str[index-1] == '?' ||
+                        str[index-1] == '"' ||
+                        str[index-1] == '(' ||
+                        str[index-1] == ')' ||
+                        str[index-1] == '{' ||
+                        str[index-1] == '}')
+                str[index] -= 32;        
+    }
+    return (str);
+}
