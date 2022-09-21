1071e89584b1d7513a0a8ddca38b30d0bb482078
sierra-95 <michaelmachohi@gmail.com>
2022-09-21 01:29:09 -0700

    f

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  *_strncpy - copies string including null byte.using at most an inputted number of bytes.
+ * @dest: buffer storing string copy
+ * @src: source string
+ * @n: max  number of string
+ * Return: dest
+ * */
+ char *_strncpy(char *dest, char *src, int n)
+ {
+     int i;
+     
+     for (i = 0; i < n && src[i] != '\0'; i++)
+     dest[i] = src[i];
+     for ( ; i < n; i++)
+     dest[i] = '\0';
+     return (dest);
+ }
