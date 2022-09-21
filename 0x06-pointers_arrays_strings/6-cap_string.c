4f019dd78d6fc5b30e83c06d58698af9909da1b7
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-21 12:51:38 +0300

    k

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,19 +12,7 @@ while (str[++index])
 {
 while(!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
-        if (str[index - 1] == ' ' ||
-                        str[index-1] == '\t' ||
-                        str[index-1] == '\n' ||
-                        str[index-1] == ',' ||
-                        str[index-1] == ';' ||
-                        str[index-1] == '.' ||
-                        str[index-1] == '!' ||
-                        str[index-1] == '?' ||
-                        str[index-1] == '"' ||
-                        str[index-1] == '(' ||
-                        str[index-1] == ')' ||
-                        str[index-1] == '{' ||
-                        str[index-1] == '}')
+        if (str[index - 1] == ' ' || str[index-1] == '\t' || str[index-1] == '\n' || str[index-1] == ',' || str[index-1] == ';' || str[index-1] == '.' || str[index-1] == '!' || str[index-1] == '?' || str[index-1] == '"' || str[index-1] == '(' || str[index-1] == ')' || str[index-1] == '{' || str[index-1] == '}')
 str[index] -= 32;        
 }
 return (str);
