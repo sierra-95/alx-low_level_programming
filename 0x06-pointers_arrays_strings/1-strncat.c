9fbce0cbeaabddd944194fdf5d6e54a77ab7e96a
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-21 11:17:35 +0300

    d

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *  *_strncat - concanates but with input bytes
- * @dest: string to be appended
- * @src: string to be completed
- * @n: integer parameter to compare index to
- * Return: returns concanated string
- * */
- char *_strncat(char *dest, char *src, int n);
- {
-     int index = 0, dest_len = 0;
+*  *_strncat - concanates but with input bytes
+* @dest: string to be appended
+* @src: string to be completed
+* @n: integer parameter to compare index to
+* Return: returns concanated string
+* */
+char *_strncat(char *dest, char *src, int n)
+{
+int index = 0, dest_len = 0;
      
-     while (dest[index++])
-     dest_len++;
-     for (index = 0; src[index] && index < n; index++)
-     dest[dest_len++] = src[index];
-     return (index);
- }
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index] && index < n; index++)
+dest[dest_len++] = src[index];
+return (index);
+}
