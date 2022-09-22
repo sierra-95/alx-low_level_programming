a8c88d2e5d291a8c25e39ea8ab1a72f42a890966
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-22 10:19:56 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,16 +4,16 @@
 *@n: parameter for test
 * Return: void
 */
- void print_number(int n)
- {
-     unsigned int num = n;
+void print_number(int n)
+{
+unsigned int num = n;
      
-     if (n < 0)
-     {
-         putchar('-');
-         num = - num;
-     }
-     if ((num / 10) > 0)
-     print_number(num / 10);
-     putchar((num % 10) + '0');
- }
+if (n < 0)
+{
+putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+putchar((num % 10) + '0');
+}
