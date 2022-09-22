a7bb9ffe5a5e62654ab3b9005ecc6b48b69d6d70
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-22 09:49:16 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,7 +14,7 @@ while (str[++index1])
 for (index2 = 0; index2 <= 7; index2++)
 {
 if (str[index1] == leet[index2] || str[index1] - 32 == leet[index2])
- str[index] = index + '0';
+ str[index1] = index2 + '0';
 }
 }
 return (str);
