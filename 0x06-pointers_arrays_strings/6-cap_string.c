609f07b24416db2c22a8210d6566374be3b9ab8f
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-22 09:40:13 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ int index = 0;
     
 while (str[++index])
 {
-while(!(str[index] >= 'a' && str[index] <= 'z'))
+while( ! (str[index] >= 'a' && str[index] <= 'z'))
 index++;
         if (str[index - 1] == ' ' || str[index-1] == '\t' || str[index-1] == '\n' || str[index-1] == ',' || str[index-1] == ';' || str[index-1] == '.' || str[index-1] == '!' || str[index-1] == '?' || str[index-1] == '"' || str[index-1] == '(' || str[index-1] == ')' || str[index-1] == '{' || str[index-1] == '}')
 str[index] -= 32;        
