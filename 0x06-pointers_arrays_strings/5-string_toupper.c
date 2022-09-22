aa16291b327ad202d4ee71873b52bab094d6929b
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-22 09:36:57 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,15 +3,15 @@
 * *string_toupper - changes all lowercasee letter of a string to uppercase
 * @str: parameter
 * Return: return is a character
-* */
-char *string_toupper(char *)
+*/
+char *string_toupper(char *str)
 {
 int index = 0;
    
 while (str[index++])
 {
 if (str[index] >= 'a' && str[index] <= 'z')
-str[index] -=32;
+str[index] -= 32;
 }
 return (str);
 }
