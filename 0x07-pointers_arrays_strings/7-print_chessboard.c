708953b27f2a0ae6e234db0eab9c1648264ba8c5
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-26 19:34:21 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,12 @@ void print_chessboard(char (*a)[8])
 {
 int i, j;
 	
-for(i = 0; i <= 7; i++)
+for(i = 0; i <= 8; i++)
 {
-for (j = 0; j <= 7; j++)
+for (j = 0; j <= 8; j++)
 {
 putchar(a[i][j]);
 }
-putchar(10);
+putchar('\n');
 }
 }
