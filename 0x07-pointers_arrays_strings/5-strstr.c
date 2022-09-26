ea580a1f17fa43e623673a9cf645973a712d5c62
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-26 19:09:25 +0300

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*_strstr - first occurence
+*@haystack: main str
+*@needle: searched in haystack
+*Return: Always 0
+*/
+char *_strstr(char *haystack, char *needle)
+{
+char *str1, *str2;
+	
+while (*haystack != '\0')
+{
+str1 = haystack;
+str2 = needle;
+while (*haystack != '\0' && *str2 != '\0' && *haystack == *str2)
+{
+haystack++;
+str2++;
+}
+if (*str2 == '\0')
+return (str1);
+haystack = str + 1;
+}
+return (0);
+}
