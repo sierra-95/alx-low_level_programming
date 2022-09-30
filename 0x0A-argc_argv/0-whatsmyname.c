5ba4d053703b1009e101e51ea5abb90adcb28628
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-30 14:09:26 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+/**
+*main - carries out
+*@argc - count
+@argv - vector command
+*Return: Always zero
+*/
 int main (int argc,char *argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-  {
-    printf ("argv[%d] = %s \n", i, argv[i]);
-  }
-    return 0;
+int i;
+for (i = 0; i < argc; i++)
+{
+printf ("argv[%d] = %s \n", i, argv[i]);
+}
+return 0;
 }
     
     
