7ffd417a7408dbdd311ec898a641ec3fbe019a54
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-30 14:23:44 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - carries out
+*@argc - count
+@argv - vector command
+*Return: Always zero
+*/
+int main(int argc, char *argv[])
+{
+  int i;
+  printf("%d", argc);
+  for (i = 0; i < argc; i++)
+  {
+    printf(" %s",argv[i]);
+  }
+  printf("\n");
+  return 0;
+}
