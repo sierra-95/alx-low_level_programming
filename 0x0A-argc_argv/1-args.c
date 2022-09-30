cd558afbc7fce1647a5dc0bd0b826625dbaf91d5
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-30 14:25:31 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,12 +7,12 @@
 */
 int main(int argc, char *argv[])
 {
-  int i;
-  printf("%d", argc);
-  for (i = 0; i < argc; i++)
-  {
-    printf(" %s",argv[i]);
-  }
-  printf("\n");
-  return 0;
+int i;
+printf("%d", argc);
+for (i = 0; i < argc; i++)
+{
+printf(" %s",argv[i]);
+}
+printf("\n");
+return 0;
 }
