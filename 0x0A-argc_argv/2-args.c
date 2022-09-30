192682de6692d83cb80b83c02ac047c16311dd06
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-09-30 15:08:13 +0300

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+*main - carries out
+*@argc - count
+@argv - vector command
+*Return: Always zero
+*/
+int main(int argc, *char argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+printf("\n");
+return 0;
+}
+    
