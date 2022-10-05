aee9b3f26787d0f1e003771523616262a8e346ab
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-10-05 14:03:16 +0300

    Create main.h

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for prototypes
+*void prototypes
+*int prototypes (void)
+*int prototypes (int)
+*/
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+
+#endif /* MAIN_H */
