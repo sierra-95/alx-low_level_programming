94bba279ad439ac4654ed2afb4fbe498f23afbbf
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-10-24 21:55:07 +0300

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,8 +6,6 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
- * Description: singly linked list node structure
  */
 typedef struct list_s
 {
