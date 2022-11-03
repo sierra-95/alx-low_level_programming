ac5a4a59891572da12924d7941bdeba4b40607b4
sierra-95 <111045570+sierra-95@users.noreply.github.com>
2022-11-03 10:26:22 +0300

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fcntl.h>
+#include "holberton.h"
+#include <unistd.h>
+
+
+int create_file(const char *filename, char *text_content)
+{
+
+	int fd, sz, i;
+	char *buf;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	buf = malloc(i * sizeof(char));
+	if (buf == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	sz = write(fd, text_content, i);
+	if (sz == -1)
+		return (-1);
+	close(fd);
+	free(buf);
+	return (1);
+
+
+}
